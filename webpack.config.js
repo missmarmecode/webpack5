cf3167e56312944c53877122d16b805a1baf12ab
missmarmecode <omoicomp@gmail.com>
2022-05-19 10:26:01 +0300

    build: added and used babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,18 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader', 'sass-loader'
                 ]
-            }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['transform-class-properties']
+                    }
+                }
+            },
         ]
     }
 }
