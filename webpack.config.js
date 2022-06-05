32581a4eb18b1918814808749a9cf676facd35b5
missmarmecode <omoicomp@gmail.com>
2022-06-05 13:14:41 +0300

    build: generating index.html with HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
-        publicPath: 'dist/',
+        publicPath: '',
     },
     mode: 'none',
     module: {
@@ -45,6 +46,12 @@ module.exports = {
         }),
         new CleanWebpackPlugin({
             cleanOnceBeforeBuildPatterns: ['**/*'], // set by default
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Hello world',
+            meta: {
+                description: 'Some description',
+            },
         }),
     ],
 };
