7950cb04ae3e0546483b85eeed467f38bde0a5ef
missmarmecode <omoicomp@gmail.com>
2022-06-05 13:38:12 +0300

    build: added separate configs for prod & dev modes

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const heading = new Heading();
 heading.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('Prod mode');
+} else if (process.env.NODE_ENV === 'development') {
+    console.log('Dev mode');
+}
